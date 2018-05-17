fcf71cbd7e52b4c30ca8ee3d04d723fd2fa1157c
yangqiankun666 <1960466252@qq.com>
2018-05-17 10:52:56 +0800

    参数简化

---
+++ bookmanex01/src/main/java/cn/edu/nyist/bookman/v0/BookVo.java
@@ -0,0 +1,121 @@
+package cn.edu.nyist.bookman.v0;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class BookVo implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private int tid;
+	private String name;
+	private String descri;
+	private double price;
+	private String author;
+	private Date pubDate;
+	private String photo;
+	
+	
+
+	public BookVo() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public int getTid() {
+		return tid;
+	}
+
+
+
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getDescri() {
+		return descri;
+	}
+
+
+
+	public void setDescri(String descri) {
+		this.descri = descri;
+	}
+
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+
+	public Date getPubDate() {
+		return pubDate;
+	}
+
+
+
+	public void setPubDate(Date pubDate) {
+		this.pubDate = pubDate;
+	}
+
+
+
+	public String getPhoto() {
+		return photo;
+	}
+
+
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+}
